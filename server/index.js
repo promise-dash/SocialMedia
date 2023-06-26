47a1992884cdd69c2ec598385e1324694904e714
Promise Dash <promisedash1607@gmail.com>
2023-06-26 23:01:39 +0530

    initial server setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
+
+app.listen(3001, () => {
+    console.log('Server is running!');
+})
